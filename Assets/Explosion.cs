396023771e7d58ce5fca0e8974d02bfb74164f57
Darth_Stalin <cusima123@mail.ru>
2024-07-05 21:14:37 +0900

    add comments

--- Assets/Explosion.cs
+++ Assets/Explosion.cs
@@ -23,19 +23,19 @@ public class Exposion : MonoBehaviour
             ExplosForce();
             aus.Play();
             Destroy(gameObject);
-            Instantiate(particalExplosion, transform.position, transform.rotation);
-            Instantiate(textureExplosion, transTexture.position, transform.rotation);
+            Instantiate(particalExplosion, transform.position, transform.rotation); //создаем частицы взрыва
+            Instantiate(textureExplosion, transTexture.position, transform.rotation); //создаеи текстуры взрыва
         }
 
     public void ExplosForce()
     {
-        Collider[] col = Physics.OverlapSphere(transform.position, Radius);
+        Collider[] col = Physics.OverlapSphere(transform.position, Radius); //радиус проверки взрыва
         foreach (Collider hit in col)
         {
-            Rigidbody rg = hit.GetComponent<Rigidbody>();
+            Rigidbody rg = hit.GetComponent<Rigidbody>(); //задаем переменную компонента rigibody
             if(rg)
             {
-                rg.AddExplosionForce(Force, transform.position, Radius, 3f);
+                rg.AddExplosionForce(Force, transform.position, Radius, 3f); // для всех в заданном радиусе с ribody приеняется сила
             }
         }
     }
